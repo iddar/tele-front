0e24c92faf1cecd48f2cb0e274993353c58a22f6
Iddar Olivares <iddar@dbug.mx>
2018-10-06 22:12:38 -0500

    include fetch

--- src/App.js
+++ src/App.js
@@ -6,11 +6,32 @@ import LineChart from './components/Chart'
 import SideBar from './components/SideBar'
 import './App.css';
 
+const baseUrl = "http://fae1f6a6.ngrok.io/api/v1"
+const defaultUrl = baseUrl + "/map/?time=1420092000"
+function getData (url = defaultUrl) {
+  return fetch(url).then(res => res.json())
+}
+
 class App extends Component {
+  constructor(props, ctx) {
+    super(props, ctx)
+    this.state = {
+      list: []
+    }
+  }
+
+  componentDidMount() {
+    getData()
+      .then(({data, next}) => {
+        this.setState({list: data})
+        console.log(data[0])
+      })
+  }
+
   render() {
     return (
       <div className="site">
-        <MapContent />
+        <MapContent data={this.state.list} />
         <div className="container">
           <div className="top">
             <SideBar />
